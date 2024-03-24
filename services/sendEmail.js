345e0349199885c1e823ce8f7faa374d690480d1
SonamSherap10 <sonamserpa3231@gmail.com>
2024-03-24 08:38:27 +0545

    near completion

---
+++ services/sendEmail.js
@@ -0,0 +1,23 @@
+const { startSession } = require("mongoose")
+const nodemailer = require("nodemailer")
+require("dotenv").config()
+const sendEmail = async (options) => {
+  var transporter = nodemailer.createTransport({
+   service : 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  })
+
+  const mailOptions = {
+    from: "Sonam Sherpa",
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  }
+
+  await transporter.sendMail(mailOptions)
+} 
+
+module.exports = sendEmail
